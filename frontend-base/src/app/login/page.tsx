31d88687b437b2b3aa60e815faa51a85a4fb5f95
wesley <wesley.rochao@hotmail.com>
2025-08-19 12:58:26 -0300

    feat: update login layout and form components

--- frontend-base/src/app/login/page.tsx
+++ frontend-base/src/app/login/page.tsx
@@ -1,8 +1,11 @@
-import { FormContainer } from "./styles";
+"use client";
+import { useState } from "react";
+import { FormContainer, LoginForm } from "./styles";
 import magdaLogo from '/public/images/magda-logo.svg'
 import Image from 'next/image';
 
 export default function LoginLayout() {
+  const [register, setRegister] = useState(false);
   return (
       <section className="text-white bg-[url('/images/magda-background.svg')] h-[100vh] bg-cover bg-no-repeat flex flex-col justify-between">
           <div className="flex justify-center items-center">
@@ -10,7 +13,17 @@ export default function LoginLayout() {
           </div>
 
         <FormContainer className="text-primary-100">
-          <h1 className="font-bold text-5xl ">Log in</h1>
+          { register ? (
+            <>
+              <h1 className="font-bold text-5xl ">Log in</h1>
+              <LoginForm />
+            </>
+          ) : (
+            <>
+              <h1 className="font-bold text-5xl ">Sign up</h1>
+              <LoginForm />
+            </>
+          )}
         </FormContainer>
       </section>
   );
--- frontend-base/src/app/login/styles.tsx
+++ frontend-base/src/app/login/styles.tsx
@@ -6,41 +6,85 @@ type FormContainerProps = {
 };
 
 export const FormContainer = ({ children, className }: FormContainerProps) => {
-    return(
-        <section className={`bg-[#EDEDF4] rounded-t-[50px] p-8 ${className} shadow-[0px_0px_30px_10px]/40 shadow-black gap-10 flex-col flex`}>
-            {children}
+  return (
+    <section
+      className={`bg-[#EDEDF4] rounded-t-[50px] p-8 ${className} shadow-[0px_0px_30px_10px]/40 shadow-black gap-10 flex-col flex`}
+    >
+      {children}
+    </section>
+  );
+};
 
-            <Input placeholder="Email" icon={'mdi:email'} />
-            <Input placeholder="Password" icon={'mdi:lock'} />
-        </section>
-    )
-}
+export const LoginForm = () => {
+  return (
+    <>
+      <div className="flex flex-col gap-5">
+        <Input placeholder="Email" icon={"mdi:email"} />
+        <Input placeholder="Password" icon={"mdi:lock"} />
+        <a href="#" className="text-xl border-b-1 w-fit leading-[1]">
+          Esqueci a senha
+        </a>
+      </div>
 
-export const FormLogin = () => {
-    return(
-        <div className={``}>
-            
-        </div>
-    )
-}
+      <LoginButton />
 
-export const InputBase = () => {
+      <div className="flex items-center text-sm text-[#00000080]">
+        <span className="flex-1 h-px bg-[#00000080]"></span>
+        <span className="px-3">Faça login com</span>
+        <span className="flex-1 h-px bg-[#00000080]"></span>
+      </div>
 
-}
+      <div className="flex gap-5 m-auto">
+        <LoginAuth icon="jam:google" />
+        <LoginAuth icon="ic:outline-facebook" />
+        <LoginAuth icon="jam:apple" />
+      </div>
 
-export const InputContainer = () => {
-    return(
-        <div className={``}>
+      <div className="m-auto">
+        <span className="text-xl text-[#00000080] pr-1">
+          Não tem uma conta?
+        </span>
+        <a href="#" className="text-xl border-b-1 w-fit leading-[1]">
+          Cadastre-se
+        </a>
+      </div>
+    </>
+  );
+};
 
-        </div>
-    )
-}
+export const Input = ({
+  placeholder,
+  icon,
+}: {
+  placeholder: string;
+  icon: string;
+}) => {
+  return (
+    <div className="text-2xl text-gray-400 w-full bg-white inset-shadow-[0px_6px_10px_0px]/20 rounded-2xl inset-shadow-black flex items-center p-2 gap-1">
+      <Icon className="opacity-50" icon={icon} />
+      <input
+        type="text"
+        className="z-10 outline-none w-full"
+        placeholder={placeholder}
+      />
+    </div>
+  );
+};
 
-export const Input = ({ placeholder, icon }: { placeholder: string, icon: string }) => {
-    return(
-        <div className="text-2xl text-gray-400 w-full bg-white inset-shadow-[0px_6px_10px_0px]/20 rounded-2xl inset-shadow-black flex items-center p-2 gap-1">
-            <Icon className="opacity-50" icon={icon} />
-            <input type="text" className="z-10 outline-none w-full" placeholder={placeholder} />
-        </div>
-    )
-}
+export const LoginButton = ({}) => {
+  return (
+    <button
+      className={`bg-primary-100 text-white font-medium text-2xl p-2 rounded-2xl`}
+    >
+      Login
+    </button>
+  );
+};
+
+export const LoginAuth = ({ icon }: { icon: string }) => {
+  return (
+    <div className="text-4xl bg-primary-100 p-2 w-fit rounded-xl text-white cursor-pointer">
+      <Icon icon={icon} />
+    </div>
+  );
+};
