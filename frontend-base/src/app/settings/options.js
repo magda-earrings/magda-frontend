2969d1556d4e32d5a5bff69dd34220b3d310fdf1
wesley <wesley.rochao@hotmail.com>
2025-08-27 21:03:27 -0300

    feat: refactor settings page to use external options configuration

---
+++ frontend-base/src/app/settings/options.js
@@ -0,0 +1,25 @@
+export const options = [
+  // {
+  //   icon: "tabler:settings",
+  //   text: "Minhas Compras",
+  //   navigate: "purchases",
+  // },
+  {
+    icon: "tabler:settings",
+    text: "Redes sociais",
+    navigate: "social",
+  },
+  {
+    icon: "tabler:settings",
+    text: "Fale conosco",
+    navigate: "contact",
+  },
+];
+
+export const optionsB = [
+  {
+    icon: "tabler:settings",
+    text: "Termos de uso",
+    navigate: "terms",
+  },
+];
